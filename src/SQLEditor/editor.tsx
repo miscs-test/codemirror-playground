f3b154e90f324ef7d3d563271b103982fe91a835
baurine <2008.hbl@gmail.com>
2024-01-03 15:15:28 +0800

    try diff view

--- src/SQLEditor/editor.tsx
+++ src/SQLEditor/editor.tsx
@@ -1,7 +1,12 @@
 import React, { useRef, useEffect } from 'react';
 import { basicSetup } from 'codemirror';
 import { EditorView, keymap } from '@codemirror/view';
-import { EditorState, Prec, EditorSelection } from '@codemirror/state';
+import {
+  EditorState,
+  Prec,
+  EditorSelection,
+  Compartment,
+} from '@codemirror/state';
 import { sql } from '@codemirror/lang-sql'; // 引入语言包
 import { oneDark } from '@codemirror/theme-one-dark';
 import { autocompletion, CompletionContext } from '@codemirror/autocomplete';
@@ -21,6 +26,7 @@ import { curSQLGutter } from './extensions/current-sql';
 import { breakpointGutter } from './extensions/breakpoints';
 import { breakpointGutter2 } from './extensions/highlight-cur-sql';
 import { inlineSuggestion } from './extensions/inline-suggestion';
+import { acceptChunk, rejectChunk, unifiedMergeView } from '@codemirror/merge';
 
 // const DOC = Array(20).fill('select * from test;').join('\n');
 // const SQL_DOC = `
@@ -47,10 +53,32 @@ this is a block comment
 
 use test;
 
+added content;
+
 select * from test.tt /* this is a line comment */
+
+select * from t.tt /* this is a line comment */
 limit 100;  /* this is another line comment */
 
 `;
+
+const SQL_DOC_ORI = `
+/*
+this is a block comment
+*/
+
+use test;
+
+removed content;
+
+select * from tt.tt /* this is a line comment */
+
+select * from t.tt /* this is a line comment */
+limit 100;  /* this is another line comment */
+
+`;
+
+let unifiedMergeViewCompartment = new Compartment();
 
 const SQLEditor: React.FC = () => {
   const editorContainerRef = useRef<HTMLDivElement>(null);
@@ -90,12 +118,20 @@ const SQLEditor: React.FC = () => {
         // curSQLGutter,
         // breakpointGutter,
         // breakpointGutter2,
-        inlineSuggestion({
-          delay: 500,
-          fetchFn: async (state) => {
-            return `hello\njavascript\nworld`;
-          },
-        }),
+        // inlineSuggestion({
+        //   delay: 500,
+        //   fetchFn: async (state) => {
+        //     return `hello\njavascript\nworld`;
+        //   },
+        // }),
+        // unifiedMergeView({
+        //   original: SQL_DOC_ORI,
+        //   highlightChanges: true,
+        //   gutter: true,
+        //   syntaxHighlightDeletions: true,
+        //   mergeControls: true
+        // })
+        unifiedMergeViewCompartment.of([]),
       ],
     });
 
@@ -139,12 +175,74 @@ const SQLEditor: React.FC = () => {
     console.log({ text });
   }
 
+  function mockAI() {
+    const editor = editorInstRef.current;
+    if (!editor) return;
+
+    const sel = editor.state.selection.main;
+    if (sel.empty) {
+      window.alert('no content is selected');
+      return;
+    }
+
+    const newContent = window.prompt('enter new content:');
+    if (newContent === null) return;
+
+    const oriDoc = editor.state.doc.toString();
+
+    editor.dispatch({
+      changes: { from: sel.from, to: sel.to, insert: newContent },
+      selection: { anchor: sel.from + newContent.length },
+    });
+
+    editor.dispatch({
+      effects: unifiedMergeViewCompartment.reconfigure(
+        unifiedMergeView({
+          original: oriDoc,
+          highlightChanges: false,
+          gutter: true,
+          syntaxHighlightDeletions: true,
+          mergeControls: false,
+        })
+      ),
+    });
+  }
+
+  function acceptAI() {
+    const editor = editorInstRef.current;
+    if (!editor) return;
+
+    const ext = unifiedMergeViewCompartment.get(editor.state);
+    if (ext) {
+      acceptChunk(editor);
+      editor.dispatch({
+        effects: unifiedMergeViewCompartment.reconfigure([]),
+      });
+    }
+  }
+
+  function rejectAI() {
+    const editor = editorInstRef.current;
+    if (!editor) return;
+
+    const ext = unifiedMergeViewCompartment.get(editor.state);
+    if (ext) {
+      rejectChunk(editor);
+      editor.dispatch({
+        effects: unifiedMergeViewCompartment.reconfigure([]),
+      });
+    }
+  }
+
   return (
     <div>
-      <div>
+      <div style={{ padding: 8, display: 'flex', gap: 4 }}>
         <button onClick={clickTest}>test</button>
+        <button onClick={mockAI}>mock AI</button>
+        <button onClick={acceptAI}>Accept</button>
+        <button onClick={rejectAI}>Reject</button>
       </div>
-      <div style={{ height: 200 }} ref={editorContainerRef}></div>
+      <div style={{ height: 400 }} ref={editorContainerRef}></div>
     </div>
   );
 };
